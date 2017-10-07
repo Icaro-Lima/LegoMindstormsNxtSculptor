124da88b0d20ed8e86310201f6b228bc447d3b19
IcaroLima <icaro.lima@ccc.ufcg.edu.br>
2017-10-07 05:34:03 -0300

    Add project files.

---
+++ LegoMindstormsNxtEscultor/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace LegoMindstormsNxtEscultor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PictureBoxImage = new System.Windows.Forms.PictureBox();
+            this.ButtonLoadImage = new System.Windows.Forms.Button();
+            this.NumericUpDownWidth = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.NumericUpDownHeight = new System.Windows.Forms.NumericUpDown();
+            this.NumericUpDownDepth = new System.Windows.Forms.NumericUpDown();
+            this.ButtonStart = new System.Windows.Forms.Button();
+            this.ButtonStop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericUpDownWidth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericUpDownHeight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericUpDownDepth)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PictureBoxImage
+            // 
+            this.PictureBoxImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PictureBoxImage.Location = new System.Drawing.Point(12, 12);
+            this.PictureBoxImage.Name = "PictureBoxImage";
+            this.PictureBoxImage.Size = new System.Drawing.Size(300, 300);
+            this.PictureBoxImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PictureBoxImage.TabIndex = 0;
+            this.PictureBoxImage.TabStop = false;
+            // 
+            // ButtonLoadImage
+            // 
+            this.ButtonLoadImage.AutoSize = true;
+            this.ButtonLoadImage.Location = new System.Drawing.Point(12, 318);
+            this.ButtonLoadImage.Name = "ButtonLoadImage";
+            this.ButtonLoadImage.Size = new System.Drawing.Size(97, 23);
+            this.ButtonLoadImage.TabIndex = 1;
+            this.ButtonLoadImage.Text = "Carregar Imagem";
+            this.ButtonLoadImage.UseVisualStyleBackColor = true;
+            this.ButtonLoadImage.Click += new System.EventHandler(this.ButtonLoadImage_Click);
+            // 
+            // NumericUpDownWidth
+            // 
+            this.NumericUpDownWidth.DecimalPlaces = 1;
+            this.NumericUpDownWidth.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.NumericUpDownWidth.Location = new System.Drawing.Point(433, 12);
+            this.NumericUpDownWidth.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.NumericUpDownWidth.Name = "NumericUpDownWidth";
+            this.NumericUpDownWidth.Size = new System.Drawing.Size(55, 20);
+            this.NumericUpDownWidth.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(318, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Tamanho do material:";
+            // 
+            // NumericUpDownHeight
+            // 
+            this.NumericUpDownHeight.DecimalPlaces = 1;
+            this.NumericUpDownHeight.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.NumericUpDownHeight.Location = new System.Drawing.Point(494, 12);
+            this.NumericUpDownHeight.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.NumericUpDownHeight.Name = "NumericUpDownHeight";
+            this.NumericUpDownHeight.Size = new System.Drawing.Size(55, 20);
+            this.NumericUpDownHeight.TabIndex = 4;
+            // 
+            // NumericUpDownDepth
+            // 
+            this.NumericUpDownDepth.DecimalPlaces = 1;
+            this.NumericUpDownDepth.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.NumericUpDownDepth.Location = new System.Drawing.Point(555, 12);
+            this.NumericUpDownDepth.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.NumericUpDownDepth.Name = "NumericUpDownDepth";
+            this.NumericUpDownDepth.Size = new System.Drawing.Size(55, 20);
+            this.NumericUpDownDepth.TabIndex = 5;
+            // 
+            // ButtonStart
+            // 
+            this.ButtonStart.Location = new System.Drawing.Point(593, 564);
+            this.ButtonStart.Name = "ButtonStart";
+            this.ButtonStart.Size = new System.Drawing.Size(75, 23);
+            this.ButtonStart.TabIndex = 6;
+            this.ButtonStart.Text = "Iniciar";
+            this.ButtonStart.UseVisualStyleBackColor = true;
+            this.ButtonStart.Click += new System.EventHandler(this.ButtonStart_Click);
+            // 
+            // ButtonStop
+            // 
+            this.ButtonStop.Enabled = false;
+            this.ButtonStop.Location = new System.Drawing.Point(674, 564);
+            this.ButtonStop.Name = "ButtonStop";
+            this.ButtonStop.Size = new System.Drawing.Size(75, 23);
+            this.ButtonStop.TabIndex = 7;
+            this.ButtonStop.Text = "Parar";
+            this.ButtonStop.UseVisualStyleBackColor = true;
+            this.ButtonStop.Click += new System.EventHandler(this.ButtonStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(761, 599);
+            this.Controls.Add(this.ButtonStop);
+            this.Controls.Add(this.NumericUpDownDepth);
+            this.Controls.Add(this.ButtonStart);
+            this.Controls.Add(this.PictureBoxImage);
+            this.Controls.Add(this.NumericUpDownHeight);
+            this.Controls.Add(this.ButtonLoadImage);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NumericUpDownWidth);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericUpDownWidth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericUpDownHeight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericUpDownDepth)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PictureBoxImage;
+        private System.Windows.Forms.Button ButtonLoadImage;
+        private System.Windows.Forms.NumericUpDown NumericUpDownWidth;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown NumericUpDownHeight;
+        private System.Windows.Forms.NumericUpDown NumericUpDownDepth;
+        private System.Windows.Forms.Button ButtonStart;
+        private System.Windows.Forms.Button ButtonStop;
+    }
+}
+
---
+++ LegoMindstormsNxtEscultor/Form1.cs
@@ -0,0 +1,144 @@
+﻿using NKH.MindSqualls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using NKH.MindSqualls.MotorControl;
+
+namespace LegoMindstormsNxtEscultor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            NumericUpDownWidth.Value = (decimal)Properties.Settings.Default["MaterialWidth"];
+            NumericUpDownHeight.Value = (decimal)Properties.Settings.Default["MaterialHeight"];
+            NumericUpDownDepth.Value = (decimal)Properties.Settings.Default["MaterialDepth"];
+        }
+
+        private void ButtonLoadImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Imagem|*.png;*.bmp;*.jpg";
+            openFileDialog.FileName = (string)Properties.Settings.Default["LastImagePath"];
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                PictureBoxImage.ImageLocation = openFileDialog.FileName;
+
+                Properties.Settings.Default["LastImagePath"] = openFileDialog.FileName;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void ButtonStart_Click(object sender, EventArgs e)
+        {
+            PictureBoxImage.Enabled = false;
+            ButtonLoadImage.Enabled = false;
+            label1.Enabled = false;
+            NumericUpDownWidth.Enabled = false;
+            NumericUpDownHeight.Enabled = false;
+            NumericUpDownDepth.Enabled = false;
+            ButtonStart.Enabled = false;
+            ButtonStop.Enabled = true;
+
+            BackgroundWorker backgroundWorker = new BackgroundWorker();
+            backgroundWorker.DoWork += BackgroundWorker_DoWork;
+            backgroundWorker.RunWorkerCompleted += BackgroundWorker_RunWorkerCompleted;
+            backgroundWorker.RunWorkerAsync(new object[] { (float)NumericUpDownWidth.Value, (float)NumericUpDownHeight.Value, (float)NumericUpDownDepth.Value, PictureBoxImage.Image.Clone() });
+        }
+
+        private void BackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            object[] args = (object[])e.Argument;
+
+            Bitmap bitmap = (Bitmap)args[3];
+
+            const float CarCmByDegress = 0.1f;
+            const float BedCmByDegress = 0.1f;
+            const float DrillingCmByDegress = 0.01f;
+
+            float materialWidth = (float)args[0] / CarCmByDegress;
+            float materialHeight = (float)args[1] / BedCmByDegress;
+            float materialDepth = (float)args[2] / DrillingCmByDegress;
+
+            int drillingMaterialMultiplicator = (int)(materialDepth / 255);
+
+            NxtBrick brick = new NxtBrick(NxtCommLinkType.USB, 0);
+            NxtMotor motorCar = new NxtMotor();
+            NxtMotor motorDrilling = new NxtMotor();
+            NxtMotor motorBed = new NxtMotor();
+
+            brick.MotorA = motorCar;
+            brick.MotorB = motorBed;
+            brick.MotorC = motorDrilling;
+
+            brick.Connect();
+
+            MotorControlProxy.StartMotorControl(brick.CommLink);
+            System.Threading.Thread.Sleep(1500);
+
+            /*int drillingDepth = 0;
+            for (int i = 0; i < bitmap.Height; i++)
+            {
+                for (int j = 0; j < bitmap.Width; j++)
+                {
+                    Color color = bitmap.GetPixel(j, i);
+                    int grayScale = (int)(color.R * 0.2126 + color.G * 0.7152 + color.B * 0.0722);
+
+                    DrillingControl(brick, drillingMaterialMultiplicator, grayScale - drillingDepth);
+                }
+            }*/
+
+            DrillingControl(brick, drillingMaterialMultiplicator, -255);
+            while (!MotorControlProxy.ISMOTORREADY(brick.CommLink, MotorControlMotorPort.PortC))
+            {
+                System.Threading.Thread.Sleep(15);
+            }
+            DrillingControl(brick, drillingMaterialMultiplicator, 255);
+        }
+
+        private static int DrillingControl(NxtBrick brick, int materialMultiplicator, int pixels)
+        {
+            int offset = pixels * materialMultiplicator;
+            int power = 25;
+
+            if (offset < 0)
+            {
+                offset *= -1;
+                power += 100;
+            }
+
+            MotorControlProxy.CONTROLLED_MOTORCMD(brick.CommLink, MotorControlMotorPort.PortC, power.ToString().PadLeft(3, '0'), offset.ToString().PadLeft(6, '0'), '5');
+            return offset;
+        }
+
+        private void BackgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ButtonStop_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default["MaterialWidth"] = NumericUpDownWidth.Value;
+            Properties.Settings.Default["MaterialHeight"] = NumericUpDownHeight.Value;
+            Properties.Settings.Default["MaterialDepth"] = NumericUpDownDepth.Value;
+
+            Properties.Settings.Default.Save();
+        }
+    }
+}
